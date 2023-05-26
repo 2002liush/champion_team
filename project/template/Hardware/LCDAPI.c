7ba659bccf1eedbfebda4da7d24bae60fa8061b7
2002liush <1714571889@qq.com>
2023-05-26 18:51:28 +0800

    modify

---
+++ project/template/Hardware/LCDAPI.c
@@ -0,0 +1,566 @@
+#include "LCDAPI.h"
+#include "lcd.h"
+#include "font.h"
+#include "oledfont.h"
+#include "bmp.h"
+
+u16 BACK_COLOR, POINT_COLOR;   
+//从ILI93xx读出的数据为GBR格式，而我们写入的时候为RGB格式。
+//通过该函数转换
+//c:GBR格式的颜色值
+//返回值：RGB格式的颜色值
+u16 LCD_BGR2RGB(u16 c)
+{
+  u16  r,g,b,rgb;   
+  b=(c>>0)&0x1f;
+  g=(c>>5)&0x3f;
+  r=(c>>11)&0x1f;	 
+  rgb=(b<<11)+(g<<5)+(r<<0);		 
+  return(rgb);
+
+}
+
+
+void Gui_Circle(u16 X,u16 Y,u16 R,u16 fc) 
+{//Bresenham算法 
+    unsigned short  a,b; 
+    int c; 
+    a=0; 
+    b=R; 
+    c=3-2*R; 
+    while (a<b) 
+    { 
+        Gui_DrawPoint(X+a,Y+b,fc);     //        7 
+        Gui_DrawPoint(X-a,Y+b,fc);     //        6 
+        Gui_DrawPoint(X+a,Y-b,fc);     //        2 
+        Gui_DrawPoint(X-a,Y-b,fc);     //        3 
+        Gui_DrawPoint(X+b,Y+a,fc);     //        8 
+        Gui_DrawPoint(X-b,Y+a,fc);     //        5 
+        Gui_DrawPoint(X+b,Y-a,fc);     //        1 
+        Gui_DrawPoint(X-b,Y-a,fc);     //        4 
+
+        if(c<0) c=c+4*a+6; 
+        else 
+        { 
+            c=c+4*(a-b)+10; 
+            b-=1; 
+        } 
+       a+=1; 
+    } 
+    if (a==b) 
+    { 
+        Gui_DrawPoint(X+a,Y+b,fc); 
+        Gui_DrawPoint(X+a,Y+b,fc); 
+        Gui_DrawPoint(X+a,Y-b,fc); 
+        Gui_DrawPoint(X-a,Y-b,fc); 
+        Gui_DrawPoint(X+b,Y+a,fc); 
+        Gui_DrawPoint(X-b,Y+a,fc); 
+        Gui_DrawPoint(X+b,Y-a,fc); 
+        Gui_DrawPoint(X-b,Y-a,fc); 
+    } 
+	
+} 
+//画线函数，使用Bresenham 画线算法
+void Gui_DrawLine(u16 x0, u16 y0,u16 x1, u16 y1,u16 Color)   
+{
+int dx,             // difference in x's
+    dy,             // difference in y's
+    dx2,            // dx,dy * 2
+    dy2, 
+    x_inc,          // amount in pixel space to move during drawing
+    y_inc,          // amount in pixel space to move during drawing
+    error,          // the discriminant i.e. error i.e. decision variable
+    index;          // used for looping	
+
+
+	Lcd_SetXY(x0,y0);
+	dx = x1-x0;//计算x距离
+	dy = y1-y0;//计算y距离
+
+	if (dx>=0)
+	{
+		x_inc = 1;
+	}
+	else
+	{
+		x_inc = -1;
+		dx    = -dx;  
+	} 
+	
+	if (dy>=0)
+	{
+		y_inc = 1;
+	} 
+	else
+	{
+		y_inc = -1;
+		dy    = -dy; 
+	} 
+
+	dx2 = dx << 1;
+	dy2 = dy << 1;
+
+	if (dx > dy)//x距离大于y距离，那么每个x轴上只有一个点，每个y轴上有若干个点
+	{//且线的点数等于x距离，以x轴递增画点
+		// initialize error term
+		error = dy2 - dx; 
+
+		// draw the line
+		for (index=0; index <= dx; index++)//要画的点数不会超过x距离
+		{
+			//画点
+			Gui_DrawPoint(x0,y0,Color);
+			
+			// test if error has overflowed
+			if (error >= 0) //是否需要增加y坐标值
+			{
+				error-=dx2;
+
+				// move to next line
+				y0+=y_inc;//增加y坐标值
+			} // end if error overflowed
+
+			// adjust the error term
+			error+=dy2;
+
+			// move to the next pixel
+			x0+=x_inc;//x坐标值每次画点后都递增1
+		} // end for
+	} // end if |slope| <= 1
+	else//y轴大于x轴，则每个y轴上只有一个点，x轴若干个点
+	{//以y轴为递增画点
+		// initialize error term
+		error = dx2 - dy; 
+
+		// draw the line
+		for (index=0; index <= dy; index++)
+		{
+			// set the pixel
+			Gui_DrawPoint(x0,y0,Color);
+
+			// test if error overflowed
+			if (error >= 0)
+			{
+				error-=dy2;
+
+				// move to next line
+				x0+=x_inc;
+			} // end if error overflowed
+
+			// adjust the error term
+			error+=dx2;
+
+			// move to the next pixel
+			y0+=y_inc;
+		} // end for
+	} // end else |slope| > 1
+}
+
+
+
+void Gui_box(u16 x, u16 y, u16 w, u16 h,u16 bc)
+{
+	Gui_DrawLine(x,y,x+w,y,0xEF7D);
+	Gui_DrawLine(x+w-1,y+1,x+w-1,y+1+h,0x2965);
+	Gui_DrawLine(x,y+h,x+w,y+h,0x2965);
+	Gui_DrawLine(x,y,x,y+h,0xEF7D);
+    Gui_DrawLine(x+1,y+1,x+1+w-2,y+1+h-2,bc);
+}
+void Gui_box2(u16 x,u16 y,u16 w,u16 h, u8 mode)
+{
+	if (mode==0)	{
+		Gui_DrawLine(x,y,x+w,y,0xEF7D);
+		Gui_DrawLine(x+w-1,y+1,x+w-1,y+1+h,0x2965);
+		Gui_DrawLine(x,y+h,x+w,y+h,0x2965);
+		Gui_DrawLine(x,y,x,y+h,0xEF7D);
+		}
+	if (mode==1)	{
+		Gui_DrawLine(x,y,x+w,y,0x2965);
+		Gui_DrawLine(x+w-1,y+1,x+w-1,y+1+h,0xEF7D);
+		Gui_DrawLine(x,y+h,x+w,y+h,0xEF7D);
+		Gui_DrawLine(x,y,x,y+h,0x2965);
+	}
+	if (mode==2)	{
+		Gui_DrawLine(x,y,x+w,y,0xffff);
+		Gui_DrawLine(x+w-1,y+1,x+w-1,y+1+h,0xffff);
+		Gui_DrawLine(x,y+h,x+w,y+h,0xffff);
+		Gui_DrawLine(x,y,x,y+h,0xffff);
+	}
+}
+
+
+/**************************************************************************************
+功能描述: 在屏幕显示一凸起的按钮框
+输    入: u16 x1,y1,x2,y2 按钮框左上角和右下角坐标
+输    出: 无
+**************************************************************************************/
+void DisplayButtonDown(u16 x1,u16 y1,u16 x2,u16 y2)
+{
+	Gui_DrawLine(x1,  y1,  x2,y1, GRAY2);  //H
+	Gui_DrawLine(x1+1,y1+1,x2,y1+1, GRAY1);  //H
+	Gui_DrawLine(x1,  y1,  x1,y2, GRAY2);  //V
+	Gui_DrawLine(x1+1,y1+1,x1+1,y2, GRAY1);  //V
+	Gui_DrawLine(x1,  y2,  x2,y2, WHITE);  //H
+	Gui_DrawLine(x2,  y1,  x2,y2, WHITE);  //V
+}
+
+/**************************************************************************************
+功能描述: 在屏幕显示一凹下的按钮框
+输    入: u16 x1,y1,x2,y2 按钮框左上角和右下角坐标
+输    出: 无
+**************************************************************************************/
+void DisplayButtonUp(u16 x1,u16 y1,u16 x2,u16 y2)
+{
+	Gui_DrawLine(x1,  y1,  x2,y1, WHITE); //H
+	Gui_DrawLine(x1,  y1,  x1,y2, WHITE); //V
+	
+	Gui_DrawLine(x1+1,y2-1,x2,y2-1, GRAY1);  //H
+	Gui_DrawLine(x1,  y2,  x2,y2, GRAY2);  //H
+	Gui_DrawLine(x2-1,y1+1,x2-1,y2, GRAY1);  //V
+  Gui_DrawLine(x2  ,y1  ,x2,y2, GRAY2); //V
+}
+
+//display 16 ziti
+void Gui_DrawFont_GBK16(u16 x, u16 y, u16 fc, u16 bc, u8 *s)
+{
+	unsigned char i,j;
+	unsigned short k,x0;
+	x0=x;
+
+	while(*s) 
+	{	
+		if((*s) < 128) 
+		{
+			k=*s;
+			if (k==13) 
+			{
+				x=x0;
+				y+=16;
+			}
+			else 
+			{
+				if (k>32) k-=32; else k=0;
+	
+			    for(i=0;i<16;i++)
+				for(j=0;j<8;j++) 
+					{
+				    	if(asc16[k*16+i]&(0x80>>j))	Gui_DrawPoint(x+j,y+i,fc);
+						else 
+						{
+							if (fc!=bc) Gui_DrawPoint(x+j,y+i,bc);
+						}
+					}
+				x+=8;
+			}
+			s++;
+		}
+			
+		else 
+		{
+		
+
+			for (k=0;k<hz16_num;k++) 
+			{
+			  if ((hz16[k].Index[0]==*(s))&&(hz16[k].Index[1]==*(s+1)))
+			  { 
+				    for(i=0;i<16;i++)
+				    {
+						for(j=0;j<8;j++) 
+							{
+						    	if(hz16[k].Msk[i*2]&(0x80>>j))	Gui_DrawPoint(x+j,y+i,fc);
+								else {
+									if (fc!=bc) Gui_DrawPoint(x+j,y+i,bc);
+								}
+							}
+						for(j=0;j<8;j++) 
+							{
+						    	if(hz16[k].Msk[i*2+1]&(0x80>>j))	Gui_DrawPoint(x+j+8,y+i,fc);
+								else 
+								{
+									if (fc!=bc) Gui_DrawPoint(x+j+8,y+i,bc);
+								}
+							}
+				    }
+				}
+			  }
+			s+=2;x+=16;
+		} 
+		
+	}
+}
+//display 24 ziti
+void Gui_DrawFont_GBK24(u16 x, u16 y, u16 fc, u16 bc, u8 *s)
+{
+	unsigned char i,j;
+	unsigned short k;
+
+	while(*s) 
+	{
+		if( *s < 0x80 ) 
+		{
+			k=*s;
+			if (k>32) k-=32; else k=0;
+
+		    for(i=0;i<16;i++)
+			for(j=0;j<8;j++) 
+				{
+			    	if(asc16[k*16+i]&(0x80>>j))	
+					Gui_DrawPoint(x+j,y+i,fc);
+					else 
+					{
+						if (fc!=bc) Gui_DrawPoint(x+j,y+i,bc);
+					}
+				}
+			s++;x+=8;
+		}
+		else 
+		{
+
+			for (k=0;k<hz24_num;k++) 
+			{
+			  if ((hz24[k].Index[0]==*(s))&&(hz24[k].Index[1]==*(s+1)))
+			  { 
+				    for(i=0;i<24;i++)
+				    {
+						for(j=0;j<8;j++) 
+							{
+						    	if(hz24[k].Msk[i*3]&(0x80>>j))
+								Gui_DrawPoint(x+j,y+i,fc);
+								else 
+								{
+									if (fc!=bc) Gui_DrawPoint(x+j,y+i,bc);
+								}
+							}
+						for(j=0;j<8;j++) 
+							{
+						    	if(hz24[k].Msk[i*3+1]&(0x80>>j))	Gui_DrawPoint(x+j+8,y+i,fc);
+								else {
+									if (fc!=bc) Gui_DrawPoint(x+j+8,y+i,bc);
+								}
+							}
+						for(j=0;j<8;j++) 
+							{
+						    	if(hz24[k].Msk[i*3+2]&(0x80>>j))	
+								Gui_DrawPoint(x+j+16,y+i,fc);
+								else 
+								{
+									if (fc!=bc) Gui_DrawPoint(x+j+16,y+i,bc);
+								}
+							}
+				    }
+			  }
+			}
+			s+=2;x+=24;
+		}
+	}
+}
+
+void Gui_DrawFont_Num32(u16 x, u16 y, u16 fc, u16 bc, u16 num)
+{
+	unsigned char i,j,k,c;
+	//lcd_text_any(x+94+i*42,y+34,32,32,0x7E8,0x0,sz32,knum[i]);
+//	w=w/8;
+
+    for(i=0;i<32;i++)
+	{
+		for(j=0;j<4;j++) 
+		{
+			c=*(sz32+num*32*4+i*4+j);
+			for (k=0;k<8;k++)	
+			{
+	
+		    	if(c&(0x80>>k))	Gui_DrawPoint(x+j*8+k,y+i,fc);
+				else {
+					if (fc!=bc) Gui_DrawPoint(x+j*8+k,y+i,bc);
+				}
+			}
+		}
+	}
+}
+
+
+void Address_set(unsigned int x1,unsigned int y1,unsigned int x2,unsigned int y2)
+{ 
+	 Lcd_WriteIndex(0x2a);
+   Lcd_WriteData(x1>>8);
+   Lcd_WriteData(x1);
+   Lcd_WriteData(x2>>8);
+   Lcd_WriteData(x2);
+  
+   Lcd_WriteIndex(0x2b);
+   Lcd_WriteData(y1>>8);
+   Lcd_WriteData(y1);
+   Lcd_WriteData(y2>>8);
+   Lcd_WriteData(y2);
+
+   Lcd_WriteIndex(0x2C);					 						 
+}
+
+
+void LCD_DrawPoint(u16 x,u16 y)
+{
+	Address_set(x,y,x,y);
+	Lcd_WriteData_16Bit(POINT_COLOR); 	    
+} 
+
+void LCD_ShowChar(u16 x,u16 y,u8 num,u8 mode)
+{
+  u8 temp;
+  u8 pos,t;
+	u16 x0=x;
+	u16 colortemp=POINT_COLOR;      
+    if(x>LCD_X_SIZE-16||y>LCD_Y_SIZE-16)return;	    
+	  
+	num=num-' ';
+	Address_set(x,y,x+8-1,y+16-1);
+	if(mode)
+	{
+		for(pos=0;pos<16;pos++)
+		{ 
+			temp=asc2_1608[(u16)num*16+pos];		 //1608
+			for(t=0;t<8;t++)
+		    {                 
+		        if(temp&0x01)POINT_COLOR=colortemp;
+				else POINT_COLOR=WHITE;
+				Lcd_WriteData_16Bit(POINT_COLOR);	
+				temp>>=1; 
+				x++;
+		    }
+			x=x0;
+			y++;
+		}	
+	}else
+	{
+		for(pos=0;pos<16;pos++)
+		{
+		    temp=asc2_1608[(u16)num*16+pos];
+			for(t=0;t<8;t++)
+		    {                 
+		        if(temp&0x01)LCD_DrawPoint(x+t,y+pos);  
+		        temp>>=1; 
+		    }
+		}
+	}
+	POINT_COLOR=colortemp;	    	   	 	  
+}   
+
+
+unsigned long oled_pow(u8 m,u8 n)
+{
+	unsigned long result=1;	 
+	while(n--)result*=m;    
+	return result;
+}
+
+void LCD_Showdecimal(u8 x,u8 y,float num,u8 z_len,u8 f_len,u8 size2)
+{         	
+	u8 t,temp;
+	u8 enshow;
+	int z_temp,f_temp;      
+	z_temp=(int)num;
+	
+	for(t=0;t<z_len;t++)
+	{
+		temp=(z_temp/oled_pow(10,z_len-t-1))%10;
+		if(enshow==0 && t<(z_len-1))
+		{
+			if(temp==0)
+			{
+				LCD_ShowChar(x+(size2/2)*t,y,' ',size2);
+				continue;
+			}
+			else
+			enshow=1;
+		}
+		LCD_ShowChar(x+(size2/2)*t,y,temp+'0',size2); 
+	}
+	
+	LCD_ShowChar(x+(size2/2)*(z_len),y,'.',size2); 
+	
+	f_temp=(int)((num-z_temp)*(oled_pow(10,f_len)));
+  
+	for(t=0;t<f_len;t++)
+	{
+		temp=(f_temp/oled_pow(10,f_len-t-1))%10;
+		LCD_ShowChar(x+(size2/2)*(t+z_len)+5,y,temp+'0',size2); 
+	}
+}
+
+void showhanzi(unsigned int x,unsigned int y,unsigned char index)	
+{  
+	unsigned char i,j;
+	unsigned char *temp=hanzi;    
+    Address_set(x,y,x+31,y+31); //设置区域      
+	temp+=index*128;	
+	for(j=0;j<128;j++)
+	{
+		for(i=0;i<8;i++)
+		{ 		     
+		 	if((*temp&(1<<i))!=0)
+			{
+				Lcd_WriteData_16Bit(POINT_COLOR);
+			} 
+			else
+			{
+				Lcd_WriteData_16Bit(WHITE);
+			}   
+		}
+		temp++;
+	 }
+}
+
+//显示图片
+void showimage(const unsigned char *p) 
+{//128*128
+  int i; 
+	unsigned char picH,picL;
+//	Lcd_Clear(WHITE); 
+	
+			Address_set(100,100,219,219);
+		    for(i=0;i<120*120;i++)
+			 {	
+			 	picL=*(p+i*2);	
+				picH=*(p+i*2+1);				
+				Lcd_WriteData_16Bit(picH<<8|picL); 				
+			 }	
+}
+
+//显示数字
+//x,y
+//len
+//color
+//num(0~4294967295);	
+void LCD_ShowNum(u16 x,u16 y,unsigned long num,u8 len)
+{         	
+	u8 t,temp;
+	u8 enshow=0;
+	num=(u16)num;
+	for(t=0;t<len;t++)
+	{
+		temp=(num/oled_pow(10,len-t-1))%10;
+		if(enshow==0&&t<(len-1))
+		{
+			if(temp==0)
+			{
+				LCD_ShowChar(x+8*t,y,' ',1);
+				continue;
+			}else enshow=1; 
+		 	 
+		}
+	 	LCD_ShowChar(x+8*t,y,temp+48,1); 
+	}
+} 
+
+void picture()
+{
+	showimage(gImage_new);
+}
+
+void show_str(u8 str[],u8 len)
+{
+	u8 i = 0;
+	for(i=0;i<len;i++)
+	{
+		LCD_ShowChar(i*15,0,str[i],0); 
+	}
+}
